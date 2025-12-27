e6d6f4defdfd71d784a052230d42507e54d7c20e
venkatasairamboddupalli971-eng <venkatasairamboddupalli971@gmail.com>
2025-12-27 19:54:23 +0530

    Add files via upload

---
+++ 27-12-2025.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int n,digit,product=1;
+  scanf("%d",&n);
+  while(n>0){
+    digit=n%10;
+    product = product * digit;
+    n = n/10;
+    printf("%d",product);
+  }    
+    return 0;
+}
